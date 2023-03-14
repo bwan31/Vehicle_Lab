f0d1beb664d413ae225205b48bc246528c5201ff
emaridette <embmarmol@gmail.com>
2023-03-14 08:39:38 -0400

    ford frivolous file

---
+++ vehicle/src/main/java/vehicle/FordFrivolous.java
@@ -0,0 +1,33 @@
+package vehicle;
+
+class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying {
+
+    /** FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6. */
+    public FordFrivolous(double startingMileage) {
+       super("Ford", "Frivolous", startingMileage, 23.6, 20);
+    }
+
+    /** Defaults mileage to 0. */
+    public FordFrivolous() {
+        super("Ford", "Frivolous", 0, 23.6, 20);
+    }
+
+    public void driveAutonomously(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean canFly(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    public void fly(double miles) {
+        if (miles < 0 || miles > getRemainingRange()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
