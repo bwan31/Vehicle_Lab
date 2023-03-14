8c72410adf974e1b849b70c4f36991796c240daf
Brendon Wan <bwan70707@gmail.com>
2023-03-14 08:21:57 -0400

    finished honda

--- vehicle/src/main/java/vehicle/Car.java
+++ vehicle/src/main/java/vehicle/Car.java
@@ -42,11 +42,7 @@ abstract class Car {
     @throws IllegalArgumentException if miles is negative or if miles is
     too high given the current fuel. */
     public abstract void drive(double miles);
-<<<<<<< HEAD
-=======
     
->>>>>>> 694890d948c1ce12d383b5ddbb4735b1c91ee948
-
     /** Gives String representation of Car as
     "<make and model> (<mileage> mi)"
     Mileage should be rounded to 1 decimal place. If mileage is a whole
